fd256866bf89bd5926f3a828012be32aad54020f
Anand S <signupwithanand@gmail.com>
2024-03-14 10:59:31 +0530

    file updated

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages,setup
 setup(
     name='mcqgenrator',
     version='0.0.1',
-    author='sunny savita',
-    author_email='sunny.savita@ineuron.ai',
+    author='Anand Subramanian',
+    author_email='signupwithanand@gmail.com',
     install_requires=["openai","langchain","streamlit","python-dotenv","PyPDF2"],
     packages=find_packages()
 )
